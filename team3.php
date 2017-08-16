6e6b5897e7731f72b00147c1af475c45976340c6
sdeepanshu02 <U15CO061@coed.svnit.ac.in>
2017-08-16 19:21:36 +0530

    update

--- team3.php
+++ team3.php
@@ -1,53 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>NIT Surat ACM</title><meta charset="utf-8" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="css/style.css" rel="stylesheet" />
+    <link rel="shortcut icon" type="image/x-icon" href="./img/favicon.ico" />
+    <!-- <script src="//use.edgefonts.net/ubuntu:n3,i3,n4,i4,n5,i5,n7,i7:all.js"></script> -->
+    <script src="http://malsup.github.io/jquery.cycle.all.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="HandheldFriendly" content="true">
 
-    <!DOCTYPE html>
-   <html>
-     <head>
-       <meta charset="utf-8">
-       <title>NIT Surat ACM</title><meta charset="utf-8" />
-       <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <link href="../css/style.css" rel="stylesheet" />
-      <link rel="shortcut icon" type="image/x-icon" href="./img/favicon.ico" />
-      <!-- <script src="//use.edgefonts.net/ubuntu:n3,i3,n4,i4,n5,i5,n7,i7:all.js"></script> -->
-       <script src="http://malsup.github.io/jquery.cycle.all.js"></script>
-       <script src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
-       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-       <meta name="viewport" content="width=device-width">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-   <meta name="HandheldFriendly" content="true">
-   <style media="screen">
-
-   @media only screen and (max-width: 500px) {
-
-     .body {
-       width: auto !important;
-     }
-
-     #about{
-       padding: 0px;
-         margin: 20px;
-         display: flex;
-         justify-content: center;
-     }
-
-     #svnitlogo , #acmlogo{
-       height: 60px;
-       width: 60px;
-       float: left;
-     }
-   }
-   .js-reveal {
-     opacity: 1;
-     transition: opacity .7s ease-in, margin-top .7s ease-out;
-   }
-
-   .js-reveal--hidden {
-     opacity: 0;
-     margin-top: 3em;
-   }
-   .block {
+<style type="text/css">
+    .block {
         text-align: center;
         vertical-align: middle;
     }
@@ -96,54 +67,54 @@
     .social {
       text-decoration: none;
       padding: 10px;
-   </style>
-   <script>
+  </style>
+
+    <script>
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-       document.getElementById("myBtn").style.display = "block";
-   } else {
-       document.getElementById("myBtn").style.display = "none";
-   }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-   document.body.scrollTop = 0;
-   document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 </script>
-</head>
-<body>
-  <div class="body">
+  </head>
+  <body>
+    <div class="body">
   <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fa fa-chevron-up" style="color: white; font-size: 1.5em"></i></button>
     <div id="thecontainer">
     <div id="logohead" class="middle">
       <div >
-        <img id="acmlogo" src="../img/resized_logo.png" alt="">
+        <img id="acmlogo" src="img/resized_logo.png" alt="">
       </div>
       <div id="acmtext">
         Association for Computing Machinery NIT Surat Student Chapter
       </div>
       <div >
-        <img  id="svnitlogo" src="../img/svnit-logo.png" alt="">
+        <img  id="svnitlogo" src="img/svnit-logo.png" alt="">
       </div>
       <div class="clear"></div>
     </div>
     <div class="fullhr"></div>
     <div id="headerblock" class="middle">
       <ul id="navigationpanel">
-        <li id="0" class="ac"><a href="./index.html">Home</a></li>
+        <li id="0"><a href="./index.html">Home</a></li>
         <li id="1"><a href="./events.html">Events</a></li><!-- place event archives here as well -->
-        <li id="2"><a href="./team.html">Team</a></li>
+        <li id="2" class="ac"><a href="./team.html">Team</a></li>
         <li id="4"><a href="./projects.html">Projects</a></li>
         <li id="5"><a href="./contact.html">Contact</a></li>
       </ul>
     </div>
   </div>
-
     
     <div id="titlebar">
       <div id="titlebartext">
@@ -276,7 +247,7 @@ function topFunction() {
         ?>
   </div>
 
-  <div class="footer-content">
+<div class="footer-content">
     <div class="f1">
       <h3 style="font-size: 1.15em; margin: 0">ACM - NIT,  SURAT</h3>
       <br>
@@ -314,3 +285,35 @@ function topFunction() {
 
     </div>
   </div>
+<div class="copyleft"><i class="fa fa-copyright fa-flip-horizontal"></i>
+  Copyleft ACM NIT SURAT
+</div>
+
+
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="js/jquery.scroll-reveal.js"></script>
+<script>
+$.fn.scrollReveal();
+</script>
+<script type="text/javascript">
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+  </body>
+</html>
